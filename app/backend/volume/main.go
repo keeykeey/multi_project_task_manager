54350f5fcb7ad21f70ca2f96398aec1b700d5d1c
keeykeey <sh1n_fuj1to@icloud.com>
2021-06-06 03:32:11 +0900

    refactored main.go and made a little change to some files

--- app/backend/volume/main.go
+++ app/backend/volume/main.go
@@ -1,99 +1,15 @@
 package main
 
 import (
-        "encoding/json"
         "log"
-        "fmt"
         "net/http"
-        _ "github.com/lib/pq"
         "mylib/db"
 )
 
-func GETHandler(w http.ResponseWriter,r *http.Request){
-        con := db.ConnectDb()
-
-        rows,err := con.Query("SELECT * FROM users;")
-        if err != nil {
-                log.Fatal(err)
-        }
-
-        type Users struct {
-                Id string
-                Name string
-                Password string
-        }
-
-        var list [] Users
-
-        for rows.Next(){
-                var user Users
-                er := rows.Scan(&user.Id,&user.Name,&user.Password)
-                if er != nil {
-                        panic(er)
-                }
-                list = append(list,user)
-        }
-
-        json_response, _ := json.MarshalIndent(list,"","\t")
-
-        w.Header().Set("Content-Type","application/json")
-        w.Write(json_response)
-
-        defer rows.Close()
-        defer con.Close()
-}
-
-func POSTHandler(w http.ResponseWriter,r *http.Request){
-        con := db.ConnectDb()
-
-        type Users struct {
-                Id string
-                Name string
-                Password string
-        }
-
-        var u Users
-        err := json.NewDecoder(r.Body).Decode(&u)
-        if err != nil {
-                http.Error(w, err.Error(),http.StatusBadRequest)
-                return
-        }
-
-        sqlStatement := `INSERT INTO users (name,password) VALUES ($1,$2) `
-        _, err = con.Exec(sqlStatement,u.Name,u.Password)
-        if err != nil {
-                w.WriteHeader(http.StatusBadRequest)
-                panic(err)
-        }
-
-        w.WriteHeader(http.StatusOK)
-        defer con.Close()
-}
-
 func main(){
-        http.HandleFunc("/",GETHandler)
-        http.HandleFunc("/post", POSTHandler)
+        http.HandleFunc("/users",db.GetUsers)
+        http.HandleFunc("/projects",db.GetProjects)
+        http.HandleFunc("/tasks",db.GetTasks)
+        http.HandleFunc("/post", db.POSTHandler)
         log.Fatal(http.ListenAndServe(":8080",nil))
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
--- app/initdb/setup.sql
+++ app/initdb/setup.sql
@@ -1,35 +1,38 @@
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY ,
+  id CHAR(4) NOT NULL ,
   name VARCHAR NOT NULL ,
-  password VARCHAR NOT NULL
+  password VARCHAR NOT NULL,
+  PRIMARY KEY (id)
 );
 
-INSERT INTO users(name, password) VALUES
-  ('testuser1', 'pwoftest1'),
-  ('testuser2', 'pwoftest2'),
-  ('testuser3', 'pwoftest3'),
-  ('testuser4', 'pwoftest4'),
-  ('testuser5', 'pwoftest5')
+INSERT INTO users(id,name, password) VALUES
+  ('0001','testuser1', 'pwoftest1'),
+  ('0002','testuser2', 'pwoftest2'),
+  ('0003','testuser3', 'pwoftest3'),
+  ('0004','testuser4', 'pwoftest4'),
+  ('0005','testuser5', 'pwoftest5')
 ;
 
 CREATE TABLE projects (
-  id SERIAL PRIMARY KEY,
+  id CHAR(4) NOT NULL,
   name VARCHAR NOT NULL,
+  PRIMARY KEY (id)
 );
 
-INSERT INTO projects(name) VALUES
-  ('sound_create'),
-  ('count_infected'),
-  ('task_manager')
+INSERT INTO projects(id,name) VALUES
+  ('0001','sound_create'),
+  ('0002','count_infected'),
+  ('0003','task_manager')
 ;
 
 CREATE TABLE tasks (
-  id SERIAL PRIMARY KEY,
+  id CHAR(4) NOT NULL,
   name VARCHAR NOT NULL,
-)
+  PRIMARY KEY (id)
+);
 
-INSERT INTO tasks(name) VALUES
-  ('create go-api'),
-  ('create frontend with ts'),
-  ('learn postgresql')
+INSERT INTO tasks(id,name) VALUES
+  ('0001','create go-api'),
+  ('0002','create frontend with ts'),
+  ('0003','learn postgresql')
 ;
