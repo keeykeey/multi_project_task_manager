0e0e5bac9608ee46616c2bcd965e3bda5b779054
keeykeey <sh1n_fuj1to@icloud.com>
2021-06-05 23:36:19 +0900

    created db package

--- app/backend/src/mylib/db/connect_db.go
+++ app/backend/src/mylib/db/connect_db.go
@@ -8,6 +8,13 @@ import (
         _ "github.com/lib/pq"
 )
 
+const (
+        db_host = "db"
+        db_port = 5432
+        db_user = "keeykeey"
+        db_name = "keeykeey"
+)
+
 func ConnectDb() *sql.DB{
         info := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",db_host,db_port,db_user,db_name)
         db, err := sql.Open("postgres",info)
