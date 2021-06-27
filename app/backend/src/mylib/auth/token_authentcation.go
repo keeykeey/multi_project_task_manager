23fcb88a39fea957836e35702b3f0401eb649a43
keeykeey <sh1n_fuj1to@icloud.com>
2021-06-27 23:47:57 +0900

    wrote login

--- app/backend/src/mylib/auth/token_authentcation.go
+++ app/backend/src/mylib/auth/token_authentcation.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"fmt"
+	"strconv"
 	"encoding/json"
 	"time"
 	"net/http"
@@ -14,35 +16,54 @@ func TokenAuthenteicate(w http.ResponseWriter, r *http.Request){
 		１）　dbのtokensテーブルに1レコード追加しtoken情報を保存し、のちにstatefulな通信ができるようにする
 		２）　HttpOnlyでcookieにtokenをつけて返す。
 	*/
+	/*
+		cors対策に、各種設定。
+		https://developer.mozilla.org/ja/docs/Web/HTTP/CORS
+	　　 から色々調べる。Access-Control-Allow-Headersにr.Header.Get("Access-Control-Request-Headers"を設置するあたり、もう少し理解深めたい。
+		どうやら単純リクエストではなく、プリフライトリクエストとなっているようで、Origin,Method,Headersを設定しなくてはいけないよう。
+		Credentialsは、front側でcredentialsを'include'に設定しているため、backend側で’Credentials:true'にしてあげなくてはいけない。
+	*/
+	w.Header().Set("Access-Control-Allow-Origin","http://127.0.0.1:3000")
+	w.Header().Set("Access-Control-Allow-Method","POST")
+	w.Header().Set("Access-Control-Allow-Credentials","true")
+	headers := r.Header.Get("Access-Control-Request-Headers")
+	w.Header().Set("Access-Control-Allow-Headers",headers)
+
 	con := db.ConnectDb()
 
+	/*
+		username と password でuserテーブルからidを含むレコードを取得するために、インターフェース（requestのjsonとsqlクエリで使う語との）を定義
+	*/	
 	type Users struct {
 		Name string
 		Password string
 	}
 
 	var u Users
+	
 	err := json.NewDecoder(r.Body).Decode(&u)
+
 	if err != nil {
-		http.Error(w, err.Error(),http.StatusBadRequest)
+		//http.Error(w, err.Error(),http.StatusBadRequest)
+		w.WriteHeader(http.StatusOK)
+		//panic(err)
+		fmt.Fprintf(w,"display error...\n" + err.Error())
 		return
-	}
+	}//エラーハンドリングをもう少し練りたいところ。フロント側で、status-codeの値に応じて処理を分岐したいので、エラーの時には返すstatus-codeを指定したい。
+	 //どうやら、json.NewDecoder(r.Body).Decode(&u)	でusernameとpasswordが正しく格納されても、errorを返してしまうようだ。
+	 //（ユーザーが正しい入力をした時に例外処理に入ってしまう）
+	
+	var query1 = "SELECT id FROM users WHERE name = $1 AND password = $2"
+	rows := con.QueryRow(query1,u.Name,u.Password)
 
-	var query1 = "SELECT id FROM users WHERE name = $1 AND password = $2;"
-
-	uid, err := con.Query(query1,u.Name,u.Password)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		panic(err)
-	}
+	/*
+		username と password でuserテーブルからidを含むレコードを取得したので、レコードからidを取り出すためにインターフェース（と言ってもただのint型変数）を定義
+	*/
+	var uid int
+	rows.Scan(&uid)
 
 	token := TokenGenerator(8)
-
 	expire := time.Now().Add(2 * time.Minute)//2分で有効期限が切れるトークンにする
-
-	var query2 = "INSERT INTO tokens(token,expire,userid) VALUES($1,$2,$3);"
-	con.Exec(query2,token,expire,uid)
-
 	c := &http.Cookie{
 		Name: "token",
 		Value: token,
@@ -50,8 +71,12 @@ func TokenAuthenteicate(w http.ResponseWriter, r *http.Request){
 		HttpOnly: true,
 	}
 
+	var query2 = "INSERT INTO tokens(token,expire,userid) VALUES($1,$2,$3)"
+	con.Exec(query2,token,expire,uid)
+
 	http.SetCookie(w,c)
-	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w,strconv.Itoa(uid))
 
 	defer con.Close()
 }
---
+++ app/frontend/src/Login.tsx
@@ -0,0 +1,93 @@
+import React, { useState,ChangeEvent }from 'react';
+import './App.css';
+import {FaEye} from 'react-icons/fa'
+
+
+interface Props{
+    id: number;
+    name: string;
+}
+
+interface Param  {
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    mode: 'no-cors' | 'cors' | 'same-origin',
+    credentials: 'include' | 'same-origin' | 'same-origin' | 'omit',
+    headers:{'Content-Type':'application/json' | 'text/html' |'multipart/form-data'},
+    body: string
+}
+
+interface InputForm {
+    name: string;
+    password: string;
+}
+  
+function Login(props:Props){
+    const [userName,setUserName] = useState<string>('testuser1')
+    const [userPassword,setUserPassword] = useState<string>('pwoftest1')
+    const [pwHiding,setPwHiding] = useState<boolean>(true)
+
+    type Mode = 'name' | 'password'
+
+    function handleInputForm(event:ChangeEvent<HTMLInputElement>,mode:Mode){
+        //https://qiita.com/natsuhiko/items/5d2a526a217e05162a0a
+        if (mode==='name'){
+            setUserName(event.target.value)
+        }else if(mode==='password'){
+            setUserPassword(event.target.value)
+        }
+    }
+
+    function handlePwHiding(){
+        switch (pwHiding){
+            case true:
+                setPwHiding(false);
+                break;
+            case false:
+                setPwHiding(true);
+                break;
+        }
+    }
+
+    function userLogin(){
+        const url: string = 'http://127.0.0.1:8080/auth'
+        const input_form:InputForm = {
+            name: userName,
+            password: userPassword
+        }
+        
+        const param:Param = {
+            method:'POST',
+            mode:'cors',
+            credentials:'include',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(input_form)
+        }
+
+        fetch(url,param)
+        .then(res=>{
+            console.log('see if fetch succeeded...',res)
+        })
+    }
+
+    return(
+        <div>
+            <h1>Login Page</h1><hr/>
+            <button onClick={()=>console.log('post me')}>POST</button>
+            
+            <ul>
+                <input onChange={(e)=>handleInputForm(e,'name')} 
+                       placeholder='name'/><br/>
+                <input type={pwHiding?'password':'text'} 
+                       onChange={(e)=>handleInputForm(e,'password')}
+                       placeholder='password'/>
+                    <FaEye className='FaEye' onClick={handlePwHiding}/><br/>
+                <button onClick={userLogin}>Login</button>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default Login
