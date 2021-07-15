263af425b0074f5872812df28ed22ada0392fd62
keeykeey <sh1n_fuj1to@icloud.com>
2021-07-16 02:06:13 +0900

    CreateTaskButton

--- app/frontend/src/components/CreateTasksButton.tsx
+++ app/frontend/src/components/CreateTasksButton.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './components.css';
 
+/*
+  PROPS
+*/
 interface Props{
-  number: number;
+  userid:number
+  projectid: number|null;
+  editInfoTrigger:Function;
 }
 
+/*
+  INTERFACE
+*/
+interface newTasks{
+  text:string;
+  deadline:string;
+  taskpriority:number;
+}
+
+
 const CreateTasksButton: React.FC<Props> = (props) => {
+  const [modalMode,setModalMode] = useState<boolean>(false)
+  function changeModalMode(){
+    switch (modalMode){
+      case true:
+        setModalMode(false);
+        console.log('true');
+        break;
+      case false:
+        setModalMode(true);
+        console.log('false');
+        break;
+    };
+  }
+
   function create(){
     console.log('create')
+    //const t:newTasks
   }
+
  
   return (
-      <button className='CreateTasksButton' onClick={create}>CREATE </button>
+    <div className='CreateTasksButtonBlock'>
+      <button className='CreateTasksButton' onClick={changeModalMode}>NEW </button>
+    </div>
   );
 }
 
