bdcf263741c7efdcf46a41e17ec3b41f4fedc9ea
keeykeey <sh1n_fuj1to@icloud.com>
2021-07-24 21:08:00 +0900

    ボタンクリック時のマウスの位置に応じて、モーダル画面の表示位置を変更

--- app/frontend/src/components/ProjectButton.tsx
+++ app/frontend/src/components/ProjectButton.tsx
@@ -35,14 +35,28 @@ const ProjectButton: React.FC<Props> = (props) => {
     height:'30px',
   }
 
-  const cssBtn:React.CSSProperties={
+  const cssEditBtn:React.CSSProperties={
     width:'50px',
     height:'30px',
+    margin:'0px auto 0px 5px',
     backgroundColor:'#5498ab99',
     borderRadius:'10px',
     border:'none',
     color:'#ffffff',
     font:'bold',
+    cursor:'pointer',
+  }
+
+  const cssDeleteBtn:React.CSSProperties={
+    width:'50px',
+    height:'30px',
+    margin:'0px auto 0px 5px',
+    backgroundColor:'#aa5555',
+    borderRadius:'10px',
+    border:'none',
+    color:'#ffffff',
+    font:'bold',  
+    cursor:'pointer',  
   }
 
   /*
@@ -64,15 +78,22 @@ const ProjectButton: React.FC<Props> = (props) => {
       PROJECT名変更用のモーダルwindow
   */
   const [modalStyle,setModalStyle] = useState<React.CSSProperties>()
-  function giveModalStyle(e:any){//本当はanyではなく、型を明示するべき
-    const _x = e.clientX
+  function giveModalStyle(e:any){　//本当は（MouseEventなど）明確な型を指定するべき
     const _y = e.clientY
+    const _x = e.clientX
+    const width:number = 250
+    const height:number = 180
+    var _yTooBig:0|1 =0
+    if (_y>(window.innerHeight/2)){
+      _yTooBig=1
+    }
+
     const style:React.CSSProperties={
-      top:_y,//クリック位置より少しずらしてモーダルを表示した方が、操作性が快適だと思うため-3した
+      top:_y-(_yTooBig*height),//クリック位置より少しずらしてモーダルを表示した方が、操作性が快適だと思うため-3した
       left:_x,//クリック位置より少しずらしてモーダルを表示した方が、操作性が快適だと思うため-3した
       position:'fixed',
-      width:'250px',
-      height:'150px',
+      width:width,
+      height:'180px',
       backgroundColor:'#ffffff',
       borderRadius:'20px 20px 20px 20px',
       alignItems:'left'
@@ -105,6 +126,59 @@ const ProjectButton: React.FC<Props> = (props) => {
     .then(res=>switchModalMode())
   }
 
+  /*
+      削除確認のモーダル
+  */
+      const [l2modal,setL2modal] = useState<boolean>(false)
+      function switchL2ModalMode(){
+        switch(l2modal){
+          case true:
+            setL2modal(false)
+            break;
+          case false:
+            setL2modal(true)
+            break;
+        }
+      }
+
+      const [cssDeleteAlert,setCssDeleteAlert] = useState<React.CSSProperties>()
+      function giveL2ModalStyle(e:any){
+        const _x:number = e.clientX
+        const _y:number = e.clientY
+        const style:React.CSSProperties={
+          position:'fixed',
+          left:_x+2,
+          top:_y+2,
+          width:'200px',
+          height:'80px',
+          backgroundColor:'#eeeeee',
+          borderRadius:'10px',
+          font:'14px bold',  
+        }
+        setCssDeleteAlert(style)
+      }
+
+      function deleteProject(){
+        const url:string='http://127.0.0.1:8080/deleteprojects'
+        const param:Param={
+          method:'DELETE',
+          mode:'cors',
+          credentials:'include',
+          headers:{
+            'projectid':String(props.project_id)
+          },
+          body:null,
+        }
+
+        fetch(url,param)
+        .then(res=>console.log('success'))
+        .then(res=>{
+          switchL2ModalMode()
+          switchModalMode()})
+        .then(res=>props.refetch_projects())
+        .catch(err=>console.log('err',err))
+      }
+    
   return (
     <div>
       <button className='ProjectButton' 
@@ -120,10 +194,30 @@ const ProjectButton: React.FC<Props> = (props) => {
         <div style={modalStyle} onClick={(e)=>e.stopPropagation()}>
           <p>タスク名を変更</p>
           <input type='text' placeholder={props.project_name} 
+                 style={cssInputForm}
                  onChange={(e)=>changeProjectName(e)}/>
-          <button style={cssBtn} onClick={editProjectName}>Send</button>
-        </div>
+          <button style={cssEditBtn} onClick={editProjectName}>Send</button>
+          <p>タスクを削除 
+            <button style={cssDeleteBtn} 
+                    onClick={(e)=>{
+                      switchL2ModalMode()
+                      giveL2ModalStyle(e)
+                    }}>Delete</button>
+          </p>
+          {/*  l2modalのtrue,falseにより、警告（本当に削除して良いか）出す出さないの表示を切り替え  */}
+            {l2modal?
+              <div id='overlay1' onClick={switchL2ModalMode}>
+              <div style={cssDeleteAlert} onClick={(e)=>e.stopPropagation()}>
+                <p>本当に削除しますか？</p>
+                <button style={cssDeleteBtn} onClick={deleteProject}>yes</button>
+                <button style={cssDeleteBtn} onClick={switchL2ModalMode}>no</button>
+              </div>
+              </div>:
+              ''            
+            }
+          {/*  l2modalのtrue,falseにより、警告（本当に削除して良いか）出す出さないの表示を切り替え  */}  
 
+        </div>
       </div>:
       ''}
 
