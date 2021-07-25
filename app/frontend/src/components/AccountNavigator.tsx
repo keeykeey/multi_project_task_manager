6662ae5c0ad625dbae64727c7c35b2c2a9671f45
keeykeey <sh1n_fuj1to@icloud.com>
2021-07-26 00:54:16 +0900

    アカウント名変更のフロント側ロジック作成

--- app/frontend/src/components/AccountNavigator.tsx
+++ app/frontend/src/components/AccountNavigator.tsx
@@ -1,5 +1,17 @@
 import React, {useState} from "react";
 
+interface Param {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  mode: 'no-cors' | 'cors' | 'same-origin',
+  credentials: 'include' | 'same-origin' | 'same-origin' | 'omit',
+  headers:{'Content-Type':'application/json' | 'text/html' |'multipart/form-data'},
+  body: string | null
+}
+
+interface NewName{
+  name:string;
+}
+
 function AccountNavigator<React>(){
   /*
       プロフィール編集画面呼び出しのボタン
@@ -39,7 +51,7 @@ function AccountNavigator<React>(){
   }
 
   const [showAcEdit,setShowAcEdit] = useState<boolean>(false)
-  function onClick(){
+  function onClickBtn(){
     switch(showAcEdit){
       case true:
         setShowAcEdit(false);
@@ -107,6 +119,30 @@ function AccountNavigator<React>(){
     setButtonStyle(defaultButtonStyle)
   }
 
+  const [newUserName,setNewUserName] = useState<string|null>(null)
+  function handleNameInput(e:any){//本当はanyではなく、型を明示するべき
+    setNewUserName(e.target.value)
+  }
+
+  function onClickButton(){
+    const new_name:NewName = {
+      name:String(newUserName)
+    }
+
+    const url:string = 'http://127.0.0.1:8080/putusers'
+    const param:Param={
+      method: 'PUT',
+      mode: 'cors',
+      credentials: 'include',
+      headers:{'Content-Type':'application/json'},
+      body: ''  
+    }      
+    
+    fetch(url,param)
+    .then(res=>console.log('success...',res))
+    .catch(err=>console.log('err...',err))
+  }
+
   return(
     <div>
       {/*
@@ -115,16 +151,17 @@ function AccountNavigator<React>(){
       <button style={btnStyle} 
               onMouseEnter={onMouseEnterBtn}
               onMouseLeave={onMouseLeaveBtn}
-              onClick={onClick}>Profile</button>
+              onClick={onClickBtn}>Profile</button>
 
       {showAcEdit?
-        <div  id='overlay3' onClick={onClick}>
+        <div  id='overlay3' onClick={onClickBtn}>
           <div style={accountEditFormStyle} onClick={(e)=>e.stopPropagation()}>
             <p>プロフィール編集</p>
             
             <div style={indexStyle}>名前
-              <input type='text' style={inputStyle}/>
+              <input type='text' style={inputStyle} onChange={(e)=>handleNameInput(e)}/>
               <button style={buttonStyle} 
+                      onClick={onClickButton}
                       onMouseEnter={onMouseEnterButton}
                       onMouseLeave={onMouseLeaveButton}>変更を保存</button>
             </div>
